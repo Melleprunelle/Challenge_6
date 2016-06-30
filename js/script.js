88d2ff66067106b08d5ed20c0522b419606533a8
Lise Poirier <lise.p.poirier@gmail.com>
2016-06-30 16:36:25 +0200

    Compréhension du JavaScripts

--- js/script.js
+++ js/script.js
@@ -1,10 +1,43 @@
+var score = 0;
+var multi = 1;
+
 function clicker() {
-    var click = document.getElementById("clicker").innerHTML;
-    click++;
-    document.getElementById("clicker").innerHTML = click;
+    score = score + multi;
+    document.getElementById("clicker").innerHTML = score;
 }
 
-function multiplicateur() {
-    var multi = document.getElementById("multiplicateur").innerHTML;
+
+/*function incrementer() {
+    if (score >= 20 * multi * multi) {
+        multi = multi * 2;
+        document.getElementById("multiplicateur").innerHTML = "Multiplicateur x" + multi;
+        document.getElementById("prix").innerHTML = "Prix du multiplicateur : " +
+            20 * multi * multi;
+
+    } else {
+        alert("Vous être trop pauvre !")
+    }
+}*/
+
+/*function incrementer() {
+    if (score >= 5) {
+        multi = multi * 2;
+        document.getElementById("multiplicateur").innerHTML = "Multiplicateur x" + multi;
+    }
     
+    else {
+        alert("Vous êtes trop pauvre !");
+    }
+}*/
+
+function incrementer() {
+    if (score >= 5) {
+        multi = multi * 2;
+        score = score - 5;
+        document.getElementById("multiplicateur").innerHTML = "Multiplicateur x" + multi;
+    }
+    
+    else {
+        alert("Vous êtes trop pauvre !");
+    }
 }
